7325f7988b2a8da14e3a0351777c5dae4c6d3af1
omerhastaken <omerefesermet@gmail.com>
2023-06-15 00:40:04 +0300

    modified:   src/App.js 	modified:   yarn.lock

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import './App.css';
 import Login from './components/Login.js'
 import Header from './components/Header';
+import Home from './components/Home'
 
 function App() {
   return (
@@ -12,6 +13,9 @@ function App() {
           <Route exact path="/">
             <Login />  
           </Route>  
+          <Route path="/home">
+            <Home />
+          </Route>
         </Switch>  
       </Router>
     </div>
