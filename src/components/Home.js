6ecf4be3a88b5dcaab5feb574a80e6aaa68dd9da
omerhastaken <omerefesermet@gmail.com>
2023-07-11 10:12:01 +0300

    modified:   src/components/Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,63 +1,70 @@
-import styled from 'styled-components';
-import ImgSlider from './ImgSlider';
-import Viewers from './Viewers';
-import Recommends from './Recommends';
-import NewDisney from './NewDisney';
-import Originals from './Originals';
-import Trending from './Trending';
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import db from '../firebase'
-import { setMovies } from '../features/movie/movieSlice';
-import { selectUserName } from '../features/user/userSlice';
+import styled from "styled-components";
+import ImgSlider from "./ImgSlider";
+import NewDisney from "./NewDisney";
+import Originals from "./Originals";
+import Recommends from "./Recommends";
+import Trending from "./Trending";
+import Viewers from "./Viewers";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import db from "../firebase";
+import { setMovies } from "../features/movie/movieSlice";
+import { selectUserName } from "../features/user/userSlice";
 
 const Home = (props) => {
-  const dispacth = useDispatch()
-  const UserName = useSelector(selectUserName)
-  let Recommends =[];
-  let NewDisney =[];
-  let Originals =[];
-  let Trending =[];
- 
+  const dispatch = useDispatch();
+  const userName = useSelector(selectUserName);
+  let recommends = [];
+  let newDisneys = [];
+  let originals = [];
+  let trending = [];
 
   useEffect(() => {
-     db.collection('movies').onSnapshot((snapshot) => {
+    console.log("hello");
+    db.collection("movies").onSnapshot((snapshot) => {
       snapshot.docs.map((doc) => {
-        switch(doc.data().type) {
-          case 'recommend':
-            Recommends.push({id: doc.id, ...doc.data()})
+        console.log(recommends);
+        switch (doc.data().type) {
+          case "recommend":
+            recommends = [...recommends, { id: doc.id, ...doc.data() }];
             break;
-          case 'new':
-            NewDisney.push({id: doc.id, ...doc.data()})
+
+          case "new":
+            newDisneys = [...newDisneys, { id: doc.id, ...doc.data() }];
             break;
-          case 'originals':
-            Originals.push({id: doc.id, ...doc.data()})
+
+          case "original":
+            originals = [...originals, { id: doc.id, ...doc.data() }];
             break;
-          case 'trending':
-            Trending.push({id: doc.id, ...doc.data()})
+
+          case "trending":
+            trending = [...trending, { id: doc.id, ...doc.data() }];
             break;
         }
-      })
+      });
 
-      dispacth(setMovies({
-        recommend: Recommends,
-        new: NewDisney,
-        originals: Originals,
-        trending: Trending,
-      }))
-     })
-  }, [UserName])
-    return (
-        <Container>
-           <ImgSlider/>
-           <Viewers />
-           <Recommends />
-           <NewDisney />
-           <Originals />
-           <Trending />
-        </Container>
-    )
-}
+      dispatch(
+        setMovies({
+          recommend: recommends,
+          newDisney: newDisneys,
+          original: originals,
+          trending: trending,
+        })
+      );
+    });
+  }, [userName]);
+
+  return (
+    <Container>
+      <ImgSlider />
+      <Viewers />
+      <Recommends />
+      <NewDisney />
+      <Originals />
+      <Trending />
+    </Container>
+  );
+};
 
 const Container = styled.main`
   position: relative;
@@ -66,6 +73,7 @@ const Container = styled.main`
   display: block;
   top: 72px;
   padding: 0 calc(3.5vw + 5px);
+
   &:after {
     background: url("/images/home-background.png") center center / cover
       no-repeat fixed;
